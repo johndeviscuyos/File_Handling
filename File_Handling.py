13325d6458dfbe55e658917da79411779ecfae71
Devis <johndeviscuyos@gmail.com>
2024-04-22 11:52:44 +0800

    Added a code where the number.txt is open by a with code and check if its working

--- File_Handling.py
+++ File_Handling.py
@@ -3,18 +3,10 @@
 
 #Create a code that will create a text file named even.txt and will have the even integers from number.txt
 with open("number.txt", "r") as numbers:
-    try:
-        integers = [int(line.strip()) for line in numbers]
+    for integers in numbers:
+        integers= integers.strip()
+        print(integers)
 
-        even_integers = [num for num in integers if num % 2 == 0]
-
-        with open("even.txt", "w") as even:
-            for num in even_integers:
-                even.write(f"{num}\n")
-        print("Even integers have been sorted out to the created file")
-
-    except ValueError:
-        print("An Invalid Integer found in number.txt")
 
 
 #Create a code that will create a text file name odd.txt and will have the odd integers from number.txt
