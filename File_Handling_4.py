c953fbad34304c003a6894e0965ef8d98f4dd150
Devis <johndeviscuyos@gmail.com>
2024-04-22 18:36:02 +0800

    added pseudocode

---
+++ File_Handling_4.py
@@ -0,0 +1,11 @@
+
+
+#create a function
+#create a code that will open source file of 20 integers
+#create a code that will create a file name double.txt
+#create a code that will Find the square of the even integers in the source file
+#store the output in double.txt
+
+#create a code that will create a file for the cube of the odd integers in double.txt
+#create a code that will find the cube of the integers
+#store the output in triple.txt
