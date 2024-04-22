8ac0865b8b8fd13c1a7bc37e91f534bf9c3f46ac
Devis <johndeviscuyos@gmail.com>
2024-04-22 11:25:37 +0800

    Added Pseudocode

---
+++ File_Handling.py
@@ -0,0 +1,6 @@
+#Create a text file named numbers.txt
+    #Add 20 integers in the text file
+
+#Create a code that will create a text file named even.txt and will have the even integers from number.txt
+
+#Create a code that will create a text file name odd.txt and will have the odd integers from number.txt
