fbf07208501fa040b3bc550254256da7e4f1c0c1
Devis <johndeviscuyos@gmail.com>
2024-04-22 15:21:12 +0800

    added a code to arrange the students gwa from highest to lowest

--- File_Handline_2.py
+++ File_Handline_2.py
@@ -6,13 +6,11 @@
 with open('GWA.txt', 'r') as gwa_file:
     students = {}
     for line in gwa_file:
+        # Create a code that will seperate name and gwa
         name, gwa = line.strip().split(' - GWA: ')
         students[name] = float(gwa)
-        print(name)
-        print(gwa)
-
-#Create a code that will seperate name and gwa
 
 #Create a code that will arrange the names and GWA from highest to lowest
+sorted_students = sorted(students.items(), key=lambda x: x[1], reverse=True)
 
 #Print the output
