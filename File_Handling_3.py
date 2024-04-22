7bfc5883e12a3a8fdff16eebaba814925f18da75
Devis <johndeviscuyos@gmail.com>
2024-04-22 17:45:43 +0800

    Added a code foe user to input a line of texts

--- File_Handling_3.py
+++ File_Handling_3.py
@@ -7,7 +7,9 @@ def creating_file(file_name):
     with open(file_name, 'w') as file:
 #Ask user to enter a line of text
         while True:
-            text_line=input("Enter line")
+            text_line=input("Enter line: ")
             file.write(text_line)
 #Ask user if he/she will input more line of text
+
 #call the function to put it in a file
+
