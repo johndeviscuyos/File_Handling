e708ee77223eb196fd6c1c24c14b40e79900450d
Devis <johndeviscuyos@gmail.com>
2024-04-22 17:27:20 +0800

    Added a function code

--- File_Handling_3.py
+++ File_Handling_3.py
@@ -1,5 +1,8 @@
-#create a function
 
+
+
+#create a function
+def creating_file(file_name):
 #create a code to open filename
 #Ask user to enter a line of text
 #Ask user if he/she will input more line of text
