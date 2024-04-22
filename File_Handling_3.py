c39f754fc6ec48a78f672825248245d2acf20044
Devis <johndeviscuyos@gmail.com>
2024-04-22 22:38:12 +0800

    added a feature where it will speak out the entered line

--- File_Handling_3.py
+++ File_Handling_3.py
@@ -1,4 +1,5 @@
-
+import pyttsx3
+text_speech=pyttsx3.init()
 
 
 #create a function
@@ -8,10 +9,19 @@ def creating_file(file_name):
 #Ask user to enter a line of text
         while True:
             text_line=input("Enter line: ")
-            file.write(text_line)
+            file.write(text_line + "\n")
+
+            text_speech.say(text_line)
+            text_speech.runAndWait()
+
 #Ask user if he/she will input more line of text
             add_lines = input("Are there more lines? y/n: ")
             if add_lines.lower() != 'y':
                 break
+
+    line_speech=("Thank you for writing your lines")
+    text_speech.say(line_speech)
+    text_speech.runAndWait()
+    print("Thank you for writing your lines")
 #call the function to put it in a file
 creating_file('mylife.txt')
