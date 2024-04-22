ccdb389d573733183933060e9094a1e6a4cf1688
Devis <johndeviscuyos@gmail.com>
2024-04-22 13:39:33 +0800

    added pseudocode

---
+++ File_Handline_2.py
@@ -0,0 +1,10 @@
+#Create a txt file with lists of 20 students with GWA
+
+
+#Create a code that will open the txt file
+
+#Create a code that will seperate name and gwa
+
+#Create a code that will arrange the names and GWA from highest to lowest
+
+#Print the output
