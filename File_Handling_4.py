7c49f66b87eaa61d9b212a494b942276192d1396
Devis <johndeviscuyos@gmail.com>
2024-04-22 19:58:26 +0800

    added a comment to tell the file that was used

--- File_Handling_4.py
+++ File_Handling_4.py
@@ -5,10 +5,16 @@ def source_file(file_name):
     # create a code that will open source file of 20 integers
     with open("number.txt", "r") as file:
         #source file used was the number.txt that is used in File_Handling
+        try:
+            integers = [int(line.strip()) for line in file]
 
+            even_integers = [num for num in integers if num % 2 == 0]
 
-#create a code that will open source file of 20 integers
-#create a code that will create a file name double.txt
+            print(even_integers)
+        except ValueError:
+            print("Invalid")
+
+source_file("Double.txt")
 #create a code that will Find the square of the even integers in the source file
 #store the output in double.txt
 
