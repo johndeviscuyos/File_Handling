f0ef30d048e545b1e95dc53ae3586bd0ba8d5d57
Devis <johndeviscuyos@gmail.com>
2024-04-22 17:28:35 +0800

    Added a code to open filename

--- File_Handling_3.py
+++ File_Handling_3.py
@@ -4,6 +4,7 @@
 #create a function
 def creating_file(file_name):
 #create a code to open filename
+    with open(file_name, 'w') as file:
 #Ask user to enter a line of text
 #Ask user if he/she will input more line of text
 #call the function to put it in a file
